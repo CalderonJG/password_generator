a55e16ae171efa90ce648bc60783ced8e6e7598d
Chimera <j.calderon3@gmail.com>
2020-10-31 23:23:29 -0400

    updated README URL; script variable update

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is my initial attempt at creating a random password generator utilizing a mixture of alphanumeric characters and symbols.
 
-Password Generator URL: file:///C:/Users/jcald/Desktop/Homework/Homework%203/password_generator/index.html
+Password Generator URL: C:/Users/jcald/Desktop/Homework/Homework%203/password_generator/index.html
--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 // Password values
 // Alphabetical characters
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Integers
-integer = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Numbers
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // Symbols
 symbol = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
 // Variables to be used
 var respond;
-var integer;
 var lowerAlpha;
 var upperAlpha;
+var number;
 var symbol;
+// converts letters to uppercase
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+// creates a variable for uppercase conversion
+alpha2 = alpha.map(toUpper);
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
 
-generateBtn.addEventListener("click", function () {
-  pass = generatePassword ();
-  document.getElementById("password").placeholder = pass;
-});
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 //Prompt code
 // Start function to generate password
-function generatePassword() {
-  responde = parseInt(prompt("How many characters would you like your password to be? Choose between 8 and 128"));
+function writePassword() {
+  respond = parseInt(prompt("How many characters would you like your password to be? Choose between 8 and 128"));
 
   if (!respond) {
     alert("Please enter a value");
@@ -38,6 +42,10 @@ function generatePassword() {
   }
 
   else {
+    lowerAlpha = confirm("Include lowercase letters?");
+    upperAlpha = confirm("Include uppercase letters?");
+    number = confirm("Include numbers?");
+    symbol = confirm("Include symbols?");
 
   }
   // Write password to the #password input
@@ -48,6 +56,3 @@ function generatePassword() {
     passwordText.value = password;
 
   }
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
